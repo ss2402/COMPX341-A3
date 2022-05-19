27074c7de082e5b8801dc9ab9a551a1a6c8e8967
samst <samsteed4@gmail.com>
2022-05-19 19:29:35 +1200

    Updated pipeline.sh so that it checks that a custom commit message has been passed to it

--- assets/pipeline.sh
+++ assets/pipeline.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-npm install
+if [ -z "$1" ]
+	then
+		echo "No argument supplied"
+		read -n 1 -s -r -p "Press any key to exit"
+else
+	npm install
 
-#Compile the code and push any changes to GitHub if there are no compile-time errors
-npm run build && cd .. && git add . && git commit --message "$1" && git push && cd assets
+	#Compile the code and push any changes to GitHub if there are no compile-time errors
+	npm run build && cd .. && git add . && git commit --message "$1" && git push && cd assets
 
-#Serve the application locally
-npm run start
+	#Serve the application locally
+	npm run start
+fi
