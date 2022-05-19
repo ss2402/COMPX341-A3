d97b1ff3c29c867d4fc5ea84bdff1f2774499aa2
samst <samsteed4@gmail.com>
2022-05-19 18:27:02 +1200

    COMPX341-22A-A3 Commiting from CI/CD Pipeline

--- assets/pipeline.sh
+++ assets/pipeline.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+#Push any updates to GitHub before starting your application
+cd ..
+git add .
+git commit --message "COMPX341-22A-A3 Commiting from CI/CD Pipeline"
+git push
+
+#Serve the application locally
+cd assets
 npm install
 npm run build
 npm run start
