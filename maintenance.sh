70714c130055a68b2f6e24d21936fc5172e1a42d
samst <samsteed4@gmail.com>
2022-05-19 23:02:01 +1200

    Added comments to maintenance.sh

--- maintenance.sh
+++ maintenance.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#Find all .ts files in the current directory/its sub-directories
+#Then, prepend a comment to each of these files
 find . -type f -name '*.ts' -exec sh -c '
     for pathname do
         sed -i "1i//Name: Samantha Steed, Student ID: 1522578" "$pathname"
