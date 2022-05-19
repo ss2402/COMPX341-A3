dcd43b393bfc51d1e5c46c55b6bdfd47732294a1
samst <samsteed4@gmail.com>
2022-05-19 15:50:02 +1200

    Made change to bestSellersBar.tsx so that the application will not compile

--- assets/build/service-worker.js
+++ assets/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7daba7c11315f120b1dd2effd4531a2d.js"
+  "/precache-manifest.6a2ed1376a2e4d37afa93f2537011187.js"
 );
 
 self.addEventListener('message', (event) => {
