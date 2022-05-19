32123f0028f1b50eb54b8860ad812a6e75ab3591
samst <samsteed4@gmail.com>
2022-05-19 19:14:31 +1200

    Updated pipeline.sh so that custom commit message can be added

--- assets/pipeline.sh
+++ assets/pipeline.sh
@@ -3,7 +3,7 @@
 npm install
 
 #Compile the code and push any changes to GitHub if there are no compile-time errors
-npm run build && cd .. && git add . && git commit --message "COMPX341-22A-A3 Commiting from CI/CD Pipeline" && git push && cd assets
+npm run build && cd .. && git add . && git commit --message "$1" && git push && cd assets
 
 #Serve the application locally
 npm run start
